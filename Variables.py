15d442a15dcdd92afd8a5e375089240e28b95a0b
netogalindo <neto_galindo@hotmail.com>
2020-02-22 10:34:09 -0300

    Update Variables.py
    Added the last content needed to this file (I bellieve)

--- Variables.py
+++ Variables.py
@@ -1,3 +1,5 @@
+import random
+
 x = 15
 price = 9.99
 discount = 0.2
@@ -28,3 +30,32 @@ This is a multi-line comment
 This is a multi-line comment
 This is a multi-line comment
 """
+
+y = z = j = 5
+
+print(y)
+print(z)
+print(j)
+
+y = "Neto", "Jose", "Arthur" # This is a tuple
+print(y)
+print(type(y))
+
+
+def using_global_keyword():
+    global n
+    n = "Hi. I'm a global variable inside of a function"
+
+
+using_global_keyword()
+print(n)
+
+w = int(5) # This is how you specify a data type
+print(w)
+print(type(w))
+w = float(w) # This is how you convert a data type
+print(w)
+print(type(w))
+
+# This randomizes a number. You have to import random to be able to do this.
+print(random.randrange(1, 10)) # 10 does not count
