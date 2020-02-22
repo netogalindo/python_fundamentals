a7b8c0cfff9b161f6263f8c4016ad8c750690e29
netogalindo <neto_galindo@hotmail.com>
2020-02-22 16:31:01 -0300

    Update FunctionArgumentsParameters.py
    Added content to the file

--- FunctionArgumentsParameters.py
+++ FunctionArgumentsParameters.py
@@ -13,3 +13,24 @@ def say_hello(name, surname):
 say_hello("Neto", "Galindo") # Positional parameter
 
 say_hello(surname="Galindo", name="Neto") # Named arguments or keyword arguments
+
+print()
+
+
+def addition_sequence(number):
+    i = 1
+    x = 1
+    previous_number = 1
+    while i <= number:
+        if i == 1:
+            i = 2
+            x = 2
+            print(x)
+        i += 1
+        new_number = x + previous_number
+        previous_number = x
+        x = new_number
+        print(x)
+
+
+addition_sequence(5)
